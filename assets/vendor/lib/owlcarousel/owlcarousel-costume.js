ffb8e3b1147ada2338eda93a6389c4c76ba33d58
DzunN86 <nurroinz@gmail.com>
2020-03-22 02:35:20 +0700

    costume owl nav

--- assets/vendor/lib/owlcarousel/owlcarousel-costume.js
+++ assets/vendor/lib/owlcarousel/owlcarousel-costume.js
@@ -2,6 +2,13 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 20,
     responsiveClass: true,
+    dots: true,
+    nav: true,
+    navText: [
+        '<i class="fas fa-arrow-left"></i>',
+        '<i class="fas fa-arrow-right"></i>'
+
+    ],
     responsive: {
         0: {
             items: 1,
