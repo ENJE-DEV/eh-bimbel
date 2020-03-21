087a81adec7979489a0ba03f236fb2259d1506e5
DzunN86 <nurroinz@gmail.com>
2020-03-22 00:17:32 +0700

    responsive owl

--- assets/vendor/lib/owlcarousel/owlcarousel-costume.js
+++ assets/vendor/lib/owlcarousel/owlcarousel-costume.js
@@ -1,20 +1,20 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 20,
-    // responsiveClass: true,
-    // responsive: {
-    //     0: {
-    //         items: 1,
-    //         nav: true
-    //     },
-    //     600: {
-    //         items: 3,
-    //         nav: false
-    //     },
-    //     1000: {
-    //         items: 5,
-    //         nav: true,
-    //         loop: false
-    //     }
-    // }
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 4,
+            nav: true,
+            loop: false
+        }
+    }
 })
